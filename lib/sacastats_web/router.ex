6e7eb42fdf9df0884b9f2cdcdf567c6ad557341d
dm0896665 <dndmackey@gmail.com>
2021-05-23 14:36:42 -0500

    Added character route

--- lib/sacastats_web/router.ex
+++ lib/sacastats_web/router.ex
@@ -17,6 +17,7 @@ defmodule SacaStatsWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/character/:character_name", PageController, :index
   end
 
   # Other scopes may use custom stacks.
