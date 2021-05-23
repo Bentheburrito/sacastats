fe2c4f0abfac36fb922069c56368bf4127eb9a88
Ben <lolitsaburrito@gmail.com>
2021-05-23 11:41:30 -0700

    add caidata_api dep

--- config/runtime.exs
+++ config/runtime.exs
@@ -5,3 +5,5 @@ if Config.config_env() == :dev do
 end
 
 config :planetside_api, service_id: System.get_env("SERVICE_ID")
+
+config :caidata_api, :data_shortname, "data2@" <> (:inet.gethostname |> elem(1) |> List.to_string()) |> String.to_atom()
