36897cb52ab12603f1f63e31fa3939e1a52d03a3
dm0896665 <dndmackey@gmail.com>
2021-05-23 13:39:52 -0500

    character mod added

--- lib/sacastats_web/controllers/page_controller.ex
+++ lib/sacastats_web/controllers/page_controller.ex
@@ -4,4 +4,10 @@ defmodule SacaStatsWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def character(conn, %{"character_name" => name}) do
+    # Fetch character info from API and DB,
+    character_stuff = %{"name" => name}
+    render(conn, "character.html", character: character_stuff)
+  end
 end
