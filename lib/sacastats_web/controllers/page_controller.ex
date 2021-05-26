2a259460a156eb1223ed8279ea279b1be72baad4
Ben <lolitsaburrito@gmail.com>
2021-05-26 10:50:54 -0700

    Initial character session page

--- lib/sacastats_web/controllers/page_controller.ex
+++ lib/sacastats_web/controllers/page_controller.ex
@@ -4,6 +4,17 @@ defmodule SacaStatsWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+  def character(conn, %{"character_name" => name, "stat_type" => "session"}) do
+    case CAIData.API.get_session_by_name(name) do
+      {:ok, session} ->
+        character_stuff = %{"name" => name, "stat_page" => "session.html"}
+        render(conn, "characterTemplate.html", character: character_stuff, session: session)
+      :none ->
+        conn
+        |> put_flash(:error, "No session under a character with that name.")
+        |> render("index.html")
+    end
+  end
 
   def character(conn, %{"character_name" => name, "stat_type" => stat_template_name}) do
     # Fetch character info from API and DB,
