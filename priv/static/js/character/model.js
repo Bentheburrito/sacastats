7cddcb75f2d3a634095f0d3dcdeb6c0642f340b8
dm0896665 <dndmackey@gmail.com>
2022-03-27 23:25:46 -0500

    Increased Performance and added a load manager.

--- priv/static/js/character/model.js
+++ priv/static/js/character/model.js
@@ -3,9 +3,14 @@ import {
     Clock,
     DirectionalLight,
     PerspectiveCamera,
+    MeshPhongMaterial,
+    MeshLambertMaterial,
+    MeshBasicMaterial,
     Scene,
     Vector3,
     CameraHelper,
+    PCFSoftShadowMap,
+    LoadingManager,
     WebGLRenderer,
 } from "https://cdn.skypack.dev/three@0.132.2";
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js";
@@ -18,6 +23,8 @@ let directionalLight;
 let controls;
 let renderer;
 let scene;
+let MODEL_PATH = "/js/assets/models/";
+let highQuality = false; //true: constantly re-renders; false: only re-renders when camera angle changes
 
 const mixers = [];
 const clock = new Clock();
@@ -36,14 +43,17 @@ function createControls() {
     controls = new OrbitControls(camera, container);
     controls.enableZoom = false;
     controls.enablePan = false;
-    controls.enableDamping = true;
     controls.target.y = .9;
     controls.minPolarAngle = Math.PI / 2;
     controls.maxPolarAngle = Math.PI / 2;
+    if (highQuality) {
+        controls.enableDamping = true; //Goes with higher quality animation loop (continues the move action)
+    } else {
+        controls.addEventListener("change", render); //Re-render when character is moved
+    }
 }
 
 function createLights() {
-    const ambientLight = new DirectionalLight(0x222222, 28);//new HemisphereLight(0xddeeff, 0x0f0e0d, 8);
     directionalLight = new DirectionalLight(0xffffff, 1);
     updateLightPosition();
 
@@ -61,19 +71,26 @@ function createLights() {
 }
 
 function loadModels() {
+    const manager = new LoadingManager();
+
     const onLoad = (gltf, position, reflective) => {
         const model = gltf.scene;
         model.traverse(n => {
             if (n.isMesh) {
+                var prevMaterial = n.material;
                 if (n.material) {
                     n.material.metalness = 0;
                     if (!reflective) {
                         n.material.roughness = 1;
                         n.receiveShadow = true;
+                        n.material = new MeshLambertMaterial();
                     } else {
                         n.castShadow = true;
+                        n.material = new MeshPhongMaterial();
                     }
                 }
+
+                MeshBasicMaterial.prototype.copy.call(n.material, prevMaterial);
             }
 
             n.frustumCulled = false; //fixes random disapearing objects
@@ -84,38 +101,42 @@ function loadModels() {
         scene.add(model);
     };
 
-    const loader = new GLTFLoader();
+    const loader = new GLTFLoader(manager);
 
     const modelPosition = new Vector3(0, 0, 0);
 
     loader.load(
-        "/js/assets/models/VS_Sniper.glb",
+        MODEL_PATH + "VS_Sniper.glb",
         (gltf) => onLoad(gltf, modelPosition, true),
         null,
         null
     );
 
     loader.load(
-        "/js/assets/models/VS_Stealth_Base.glb",
+        MODEL_PATH + "VS_Stealth_Base.glb",
         (gltf) => onLoad(gltf, modelPosition, false),
         null,
         null
     );
 
     loader.load(
-        "/js/assets/models/VS_Infil_Armor.glb",
+        MODEL_PATH + "VS_Infil_Armor.glb",
         (gltf) => onLoad(gltf, modelPosition, true),
         null,
         null
     );
 
     loader.load(
-        "/js/assets/models/caucasianFemaleHead.glb",
+        MODEL_PATH + "caucasianFemaleHead.glb",
         (gltf) => onLoad(gltf, modelPosition, false),
         null,
         null
     );
 
+    manager.onLoad = function () {
+        update();
+        render();
+    };
     //scene.add(new CameraHelper(camera)); //shows light patterns
 }
 
@@ -129,8 +150,9 @@ function createRenderer() {
 
     renderer.physicallyCorrectLights = true;
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.gammaOutput = true;
+    renderer.outputEncoding = 3001; //(sRGBEncoding) same as `renderer.gammaOutput = true;`
     renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = PCFSoftShadowMap;
 
     // add the automatically created <canvas> element to the page
     container.appendChild(renderer.domElement);
@@ -143,10 +165,10 @@ function update() {
         mixer.update(delta);
     });
     controls.update(delta);
-    updateLightPosition();
 }
 
 function render() {
+    updateLightPosition();
     renderer.render(scene, camera);
 }
 
@@ -158,12 +180,14 @@ function updateLightPosition() {
 }
 
 function onWindowResize() {
-    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.aspect = container.clientWidth / container.clientHeight + .3;
 
     // update the camera's frustum
     camera.updateProjectionMatrix();
 
     renderer.setSize(container.clientWidth + 230, container.clientHeight + 30);
+
+    render();
 }
 
 export default function init(containerID) {
@@ -177,10 +201,13 @@ export default function init(containerID) {
     loadModels();
     createRenderer();
 
-    renderer.setAnimationLoop(() => {
-        update();
-        render();
-    });
+    if (highQuality) {
+        //higher quality, but really CPU expensive
+        renderer.setAnimationLoop(() => {
+            update();
+            render();
+        });
+    }
 
     window.addEventListener("resize", onWindowResize);
 }
