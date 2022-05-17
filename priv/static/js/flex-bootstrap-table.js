2f17c243b1a8e6ea4e6411d95271baf856ebd0a7
dm0896665 <dndmackey@gmail.com>
2022-05-17 18:45:52 -0500

    Fixed null pointer

--- priv/static/js/flex-bootstrap-table.js
+++ priv/static/js/flex-bootstrap-table.js
@@ -10,9 +10,9 @@ export function setupFlexTables() {
     document.querySelectorAll('.table-responsive-stack').forEach(table => {
         bootstrapTableFilter.init(table.id);
         bootstrapSelection.init(table.id);
+        initializeStickyHeaderWidths();
         init();
     });
-    initializeStickyHeaderWidths();
 
     function init() {
         initializeFlexTables();
