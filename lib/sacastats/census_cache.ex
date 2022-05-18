a53fbe17ef0ed636a88030bcd8611d5daec6411d
Ben <lolitsaburrito@gmail.com>
2022-05-18 13:19:10 -0700

    final updates, format/credo/tests passing

--- lib/sacastats/census_cache.ex
+++ lib/sacastats/census_cache.ex
@@ -4,19 +4,16 @@ defmodule SacaStats.CensusCache do
   """
   use GenServer
 
-  @type cache_result ::
-          {:ok, value :: any()}
-          | {:error, :not_found}
-          | {:error, HTTPoison.Error.t() | Jason.DecodeError.t() | PS2.API.Error.t()}
+  @type cache_result :: {:ok, value :: any()} | {:error, :not_found}
 
-  @type fallback_fn ::
-          (... ->
-             {:ok, PS2.API.QueryResult.t()} | {:error, any()})
+  @type fallback_fn :: (key :: any() -> value :: any() | :not_found)
 
   ### API
 
   def start_link(opts) when is_list(opts) do
-    GenServer.start_link(__MODULE__, nil, opts)
+    {fallback_fn, opts} = Keyword.pop(opts, :fallback_fn, nil)
+
+    GenServer.start_link(__MODULE__, {nil, fallback_fn}, opts)
   end
 
   @doc """
@@ -34,53 +31,34 @@ defmodule SacaStats.CensusCache do
   @doc """
   Get a value from the cache under the specified key.
 
-  Returns {:ok, value} if the key-value is present in the cache. If a fallback function is provided, it will be lazily
-  evaluated to obtain the value, and update the cache automatically. The fallback function is expected to be either
-  `c:PS2.API.query/3` or `c:PS2.API.query_one/3`, though a custom function can be provided as long as its returns are
-  the same.
-
-  If the key-value is neither in the cache nor retrieved by the fallback function, {:error, :not_found} is returned, or
-  another error tuple if returned by the fallback function.
+  Returns {:ok, value} if the key-value is present in the cache. If the key-value is not present, and a fallback
+  function is provided, it will be lazily evaluated to obtain the value, and update the cache automatically. If no
+  fallback is provided, or it returns `:not_found`, the cache will return `{:error, :not_found}`
   """
-  @spec get(
-          cache :: pid() | atom(),
-          key :: any(),
-          {fallback_fn :: fallback_fn(), args :: [any()]} | nil
-        ) :: cache_result()
-  def get(cache, key, fallback \\ nil) when is_pid(cache) or is_atom(cache) do
-    GenServer.call(cache, {:get, key, fallback})
+  @spec get(cache :: pid() | atom(), key :: any()) :: cache_result()
+  def get(cache, key) when is_pid(cache) or is_atom(cache) do
+    GenServer.call(cache, {:get, key})
   end
 
   ### Impl
 
   @impl true
-  def init(_) do
-    {:ok, %{}}
-  end
+  def init({_, nil}), do: {:ok, {%{}, fn _ -> :not_found end}}
+  def init({_, fallback_fn}), do: {:ok, {%{}, fallback_fn}}
 
   @impl true
-  def handle_call({:get, key, fallback}, _from, state) do
-    {result, new_state} =
-      case Map.fetch(state, key) do
-        {:ok, value} -> {{:ok, value}, state}
-        :error -> eval_fallback_fn(fallback, key, state)
-      end
+  def handle_call({:get, key}, _from, {state, fallback_fn}) do
+    case Map.get_lazy(state, key, fn -> fallback_fn.(key) end) do
+      :not_found ->
+        {:reply, {:error, :not_found}, {state, fallback_fn}}
 
-    {:reply, result, new_state}
-  end
-
-  @impl true
-  def handle_cast({:put, keys, value}, state) do
-    {:noreply, Enum.reduce(keys, state, &Map.put(&2, &1, value))}
-  end
-
-  defp eval_fallback_fn(nil, _key, cache_state), do: {{:error, :not_found}, cache_state}
-
-  defp eval_fallback_fn({fallback_fn, args}, key, cache_state) do
-    case apply(fallback_fn, args) do
-      {:ok, %PS2.API.QueryResult{returned: 0}} -> {{:error, :not_found}, cache_state}
-      {:ok, %PS2.API.QueryResult{data: data}} -> {{:ok, data}, Map.put(cache_state, key, data)}
-      {:error, _e} = error -> {error, cache_state}
+      value ->
+        {:reply, {:ok, value}, {Map.put_new(state, key, value), fallback_fn}}
     end
+  end
+
+  @impl true
+  def handle_cast({:put, keys, value}, {state, fallback_fn}) do
+    {:noreply, {Enum.reduce(keys, state, &Map.put(&2, &1, value)), fallback_fn}}
   end
 end
