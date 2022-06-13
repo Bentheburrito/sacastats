b10ee37096cf5779529d2eca88c49768570ca7bf
dm0896665 <46948300+dm0896665@users.noreply.github.com>
2022-06-12 22:34:07 -0500

    Character General Alpha stats (#88)
    * Got base model dynamically rendering
    
    * Dynamically rendering head and fixed syntax
    
    * NSO fix
    
    * Dynamically show character armor
    
    * Dynamically rendering weapons now
    
    * Gave more space on the sides of model to prevent gun cut off
    
    * Added MAXes to dynamic rendering
    
    * Got rid of unused navigation
    
    * Got rid of bad styling
    
    * Added character characteristic info visual with mobile support
    
    * Changed row min-width
    
    * Made model and stats look better in mobile
    
    * Made percent on progress bar more readable
    
    * Fixed toast position
    
    * Swapped to bootstrap 5 stylings
    
    * Eased in the character model and added a loader for it
    
    * Better wording
    
    * Added more info to character header
    
    * Added leader info to character header
    
    * Added spacing and updated time message
    
    * Fixed spacing on mobile
    
    * Fixed long over due format bug
    
    * Removed warning
    
    * Added more stats to pull
    
    * Added  Best Weapons, Current Medals, and General Stats
    
    * Wording and style for last updated date-time
    
    * KD Fix
    
    * Adjusted wording
    
    * Mobile fix for 'last updated'
    
    * Fixed Firefox flexbox issue

--- priv/static/js/formats.js
+++ priv/static/js/formats.js
@@ -20,20 +20,23 @@ export function addFormatsToPage() {
             let dateTimeString = dateTime.innerHTML;
             let date = dateTimeString.split(" ")[0];
             let time = dateTimeString.split(" ")[1];
-            let dateTimeObject = new Date(getLocalDateStringWithTimeFromStrings(date, time));
-            const longFormatter = new Intl.DateTimeFormat(preferedLanguage, {
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',
-            });
+            if (!isNaN(date.charAt(0))) {
+                let dateTimeObject = new Date(getLocalDateStringWithTimeFromStrings(date, time));
+                const longFormatter = new Intl.DateTimeFormat(preferedLanguage, {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                });
 
-            dateTime.innerHTML = longFormatter.format(dateTimeObject) + " @ " + dateTimeObject.toLocaleTimeString();
+                dateTime.innerHTML = longFormatter.format(dateTimeObject) + " @ " + dateTimeObject.toLocaleTimeString();
+            }
         });
     }
 
     function getLocalDateStringWithTimeFromStrings(date, time) {
         let dateArr = date.split("-");
         let timeArr = time.split(":");
+
         return new Date(Date.UTC(dateArr[0], dateArr[1] - 1, dateArr[2], timeArr[0], timeArr[1], timeArr[2].split(".")[0])).toLocaleString(preferedLanguage, { timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone });
     }
 
