55d72a4072203235a913c1daab3b56701aa80d43
dm0896665 <dndmackey@gmail.com>
2022-07-19 22:44:14 -0500

    Added new lookup frontend with test data

--- priv/static/js/character/lookup.js
+++ priv/static/js/character/lookup.js
@@ -2,22 +2,32 @@ import { show as showLoadingScreen } from "/js/loading-screen.js";
 
 window.addEventListener('load', (event) => {
     searchCharacter();
+    addCharacterCardClick();
 });
 
 function searchCharacter() {
     let btn = document.getElementById("searchButton");
     let form = document.getElementById("characterSearchForm");
+    let characterName = document.getElementById("character").value;
     form.addEventListener('submit', function (event) {
-        swapURL(event);
+        swapURL(event, characterName);
     });
     btn.addEventListener("click", function (event) {
-        swapURL(event);
+        swapURL(event, characterName);
     });
 }
 
-function swapURL(event) {
+function swapURL(event, characterName) {
     showLoadingScreen();
     event.preventDefault();
-    let character = document.getElementById("character").value;
-    location.href = document.URL + "/" + character;
+    location.href = document.URL + "/" + characterName;
+}
+
+function addCharacterCardClick() {
+    document.querySelectorAll(".character-status-card").forEach(card => {
+        card.addEventListener('click', function (event) {
+            let characterName = card.id.split("-")[0];
+            swapURL(event, characterName);
+        });
+    });
 }
