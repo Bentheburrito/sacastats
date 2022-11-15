dca1e4c25bd30f77d4a7b1822231b67f1fb933cb
dm0896665 <dndmackey@gmail.com>
2022-11-14 21:24:31 -0600

    Example with testing

---
+++ assets/src/index.ts
@@ -0,0 +1,9 @@
+export function sum(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+init();
+
+function init() {
+  sum(1, 2);
+}
---
+++ assets/test/index.test.ts
@@ -0,0 +1,6 @@
+import { sum } from '../src';
+describe('sum function', () => {
+  it('adds 2 numbers', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
