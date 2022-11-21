cc77a5e031471e184406209118a987b8f79fe8d1
dm0896665 <dndmackey@gmail.com>
2022-11-21 17:07:43 -0600

    Fixed errors

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,8 +32,8 @@ let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfTo
 
 // Show progress bar on live navigation and form submits
 topbar.topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+window.addEventListener("phx:page-loading-start", info => topbar.topbar.show())
+window.addEventListener("phx:page-loading-stop", info => topbar.topbar.hide())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
